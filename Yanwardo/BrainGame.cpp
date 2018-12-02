e070bdbb9640214322dbeb048123c2ddee57ed62
Yanwardo Sitanggang <ywdfsctgclev0301@gmail.com>
2018-12-02 17:35:17 +0700

    Create BrainGame.cpp

---
+++ Yanwardo/BrainGame.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//Manifest untuk Level
+struct manifestLevel{
+	int intervalRandomPertanyaan;
+	int jumlahJackpotPerLevel;
+	int xTiapPertanyaan;
+};
+
+
+//Manifest untuk Mode <easy, medium, hard>
+struct manifestMode{
+	int jumlahKarakter;
+	char daftarKarakter[];
+	int waktuIngat;
+	int waktuJawab;
+	int nyawa;
+};
+
+
+
+
+
+
+int main(){
+	
+	
+	
+	
+	
+	return 0;
+}
