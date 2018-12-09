99e1170295b80b3caae1328f57a7c333b32ccf2b
Yanwardo Sitanggang <ywdfsctgclev0301@gmail.com>
2018-12-09 19:44:56 +0700

    (770 byt
     (770 byt

--- Yanwardo/BrainGame.cpp
+++ Yanwardo/BrainGame.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 
 ////dataPermainan
-struct dataPermainan{
+struct dataRuntime{
+	int level;
+	int noScene;
+	int noPertanyaan;
 	int nyawa;
 	int skor;
 	bool lanjut;
@@ -19,7 +22,7 @@ struct manifestLevel{
 //yanwardo sitanggang florensius
 //Manifest untuk Mode <easy, medium, hard>
 struct manifestMode{
-	int jumlahKarakter;
+	int panjangScene;
 	char daftarKarakter[];
 	int waktuIngat;
 	int waktuJawab;
@@ -27,37 +30,82 @@ struct manifestMode{
 };
 
 
+
+
 //LIST
 //data_scene
-struct data_Scene{
+struct listScene{
 	int nomor;
+	int panjang;
 	string scene;
-	data_Scene *next;
+	listScene *next;
 };
 struct kepalaScene{
-	data_Scene *first;
+	listScene *first;
 };
 
 //data_Pertanyaan
-struct data_Pertanyaan{
+struct listPertanyaan{
 	int nomor;
+	int mutu;
 	string pertanyaan;	
-	data_Pertanyaan *next; 
+	string jawaban;
+	listPertanyaan *next; 
 };
 struct kepalaPertanyaan{
-	data_Pertanyaan *first;
+	listPertanyaan *first;
 };
 
+/*format pertanyaan
+mutu 1:
+berapa jumlah * pada scene x?
+
+mutu 2:
+berapa jumlah * pada scene x dan y?
+
+berapa jumlah * pada scene x, y, dan z?
+
+*/
 
 
-
-
+struct data{
+	string nomor;
+	int angka;
+};
 int main(){
 	manifestLevel x[3];
 	
-	x{
-	mutuPertanyaan = 5;
-	};
+	
+	manifestMode MODE[3];
+	
+	MODE[0].panjangScene=7;
+	MODE[0].daftarKarakter="@%*))";
+	MODE[0].nyawa= ;
+	MODE[0].waktuIngat= ;
+	MODE[0].waktuJawab= ;
+	
+	MODE[1].panjangScene=7;
+	MODE[1].daftarKarakter="";
+	MODE[1].nyawa= ;
+	MODE[1].waktuIngat= ;
+	MODE[1].waktuJawab= ;
+	
+	MODE[2].panjangScene=7;
+	MODE[2].daftarKarakter="";
+	MODE[2].nyawa= ;
+	MODE[2].waktuIngat= ;
+	MODE[2].waktuJawab= ;
+	
+	
+	
+	
+	data haha;
+	data *alamatHaha;
+	
+	
+	haha.angka=5;
+	haha.nomor="sesuatu";
+	
 	
 	return 0;
 }
